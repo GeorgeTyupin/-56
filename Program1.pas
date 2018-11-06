6b10f6a7ea7ce1ebd85e990fd9c261c08db303be
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2018-11-06 19:09:24 +0300

    Add files via upload

---
+++ Program1.pas
@@ -0,0 +1,19 @@
+﻿var name, verb, verbi, adjective, creature, noun, exclamation, yes: string;
+begin
+writeln('введите существительное в предложном падеже');
+readln(noun);
+writeln('введите существительное в именительном падеже');
+readln(name);
+writeln('введите прилагательное');
+readln(adjective);
+writeln('введите существительное в именительном падеже (СУШЕСТВИТЕЛЬНОЕ ДОЛЖНО БЫТЬ ТОГО ЖЕ РОДА, ЧТО И ПРИЛАГАТЕЛЬНОЕ КОТОРОЕ ТЫ ВВЁЛ');
+readln(creature);
+writeln('введите глагол');
+readln(verbi);
+writeln('введите глагол');
+readln(verb);
+writeln('введите восклицание');
+readln(exclamation);
+writeln('Однажды на конкурсе по ',noun,name,'   создал ',creature,'   .  И тогда ',creature ,verbi,' .  После этого ',name ,verb,' и воскликнул ' ,exclamation,'. После этого ' ,creature,' больше никогда не делал этого');
+writeln('Ты молодец. Хочешь пройти тест ещё раз? Если хочешь то напиши YES если нет то напиши, что хочешь');
+end.
